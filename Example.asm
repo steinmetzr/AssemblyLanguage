f4eb5175d972b80fc915dbf51cb7131e7d8cda7f
steinmetzr <steinmetzr@wit.edu>
2015-05-27 14:17:33 -0400

    Example program

---
+++ Example.asm
@@ -0,0 +1,13 @@
+TITLE Add and Subtract (AddSub.asm)
+; This program adds and subtracts 32-bit integers.
+INCLUDE Irvine32.inc
+
+.code
+     main PROC
+          mov eax,10000h ; EAX = 10000h
+          add eax,40000h ; EAX = 50000h
+          sub eax,20000h ; EAX = 30000h
+          call DumpRegs ; display registers
+          exit
+     main ENDP
+END main
